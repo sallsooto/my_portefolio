9655d36e37fc3b14b23be766590232455e4e3b62
Thierno Boubacar Sall <thierno.sall@snsoftware.com>
2021-11-01 14:59:29 +0000

    profile_picture set

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import picture from '../assets/jpeg/profile_picture.jpeg'
 
 export const headerData = {
     name: 'Thierno Boubacar Sall',
     title: "Web Developer",
     desciption:"Nothing is impossible. You have to believe in yourself. Work seriously with constancy and determination. Hold on even if it is not easy. I think great achievements take a lot of effort and sacrifice. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: picture,
     resumePdf: resume
 }
